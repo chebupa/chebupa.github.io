5d45b1a6f82745971499b4c9e02d5c2500faf34c
Chebupela <aristarhsakulin@gmail.com>
2023-07-16 13:27:30 +0300

    mobile and tablet optimization 0.1

--- js/core.js
+++ js/core.js
@@ -1,34 +1,72 @@
 // the main js file
-console.log('JS connected succesfuly')
+console.log('JS connected succesfuly');
 
 
-// ChebuCraft pop-up context menu
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
+// ChebuCraft modal window
+const chebucraftModal = document.querySelector('.chebucraft-modal');
+const chebucraftOverlay = document.querySelector('.chebucraft-overlay');
+const chebucraftOpenModalBtn = document.querySelector('.chebucraft-btn-open');
+const chebucraftCloseModalBtn = document.querySelector('.chebucraft-btn-close');
 
 // close modal function
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+function chebucraftCloseModal() {
+  chebucraftModal.classList.add('hidden');
+  chebucraftOverlay.classList.add('hidden');
 };
 
 // close the modal when the close button and overlay is clicked
-closeModalBtn.addEventListener("click", closeModal);
-// overlay.addEventListener("click", closeModal);
+chebucraftCloseModalBtn.addEventListener('click', chebucraftCloseModal);
 
 // close modal when the Esc key is pressed
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !chebucraftModal.classList.contains('hidden')) {
+    chebucraftCloseModal();
   }
 });
 
 // open modal function
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+function chebucraftOpenModal() {
+  chebucraftModal.classList.remove('hidden');
+  chebucraftOverlay.classList.remove('hidden');
 };
+
 // open modal event
-openModalBtn.addEventListener("click", openModal);
+chebucraftOpenModalBtn.addEventListener('click', chebucraftOpenModal);
+
+
+
+//! ======================================================================
+
+
+
+
+// QR Code modal window
+const qrcodeModal = document.querySelector('.qrcode-modal');
+const qrcodeOverlay = document.querySelector('.qrcode-overlay');
+const qrcodeOpenModalBtn = document.querySelector('.qrcode-btn-open');
+const qrcodeCloseModalBtn = document.querySelector('.qrcode-btn-close');
+
+// close modal function
+function qrcodeCloseModal() {
+  qrcodeModal.classList.add('hidden');
+  qrcodeOverlay.classList.add('hidden');
+};
+
+// close the modal when the close button and overlay is clicked
+qrcodeCloseModalBtn.addEventListener('click', qrcodeCloseModal);
+
+// close modal when the Esc key is pressed
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !qrcodeModal.classList.contains('hidden')) {
+    qrcodeCloseModal();
+  }
+});
+
+// open modal function
+function qrcodeOpenModal() {
+  qrcodeModal.classList.remove('hidden');
+  qrcodeOverlay.classList.remove('hidden');
+};
+
+// open modal event
+qrcodeOpenModalBtn.addEventListener('click', qrcodeOpenModal);
