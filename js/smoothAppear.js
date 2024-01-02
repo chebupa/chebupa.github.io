b98aedf5f83d75b73a40ebb32a7a60d97715d417
chebupa <aristarhsakulin@gmail.com>
2024-01-02 17:12:02 +0300

    smooth appear added

---
+++ js/smoothAppear.js
@@ -0,0 +1,17 @@
+// smooth block appear script
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        }
+    });
+}
+  
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+  
+for (let elm of elements) {
+    observer.observe(elm);
+}
